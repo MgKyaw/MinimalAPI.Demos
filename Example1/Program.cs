78ebafc162ee9e69e1ccc5678cb3eb16b844671a
MgKyaw <mgkyaw20112@gmail.com>
2025-10-22 22:55:28 +0700

    feat: Add DI service for BookService

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,4 +1,9 @@
+using Example1;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IBookService, BookService>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
