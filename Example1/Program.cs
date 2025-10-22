627dfe77c73db424b789b9460edaebf095e06087
MgKyaw <mgkyaw20112@gmail.com>
2025-10-22 22:59:44 +0700

    feat: Add /books api with service method

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,4 +1,5 @@
 using Example1;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,5 +8,15 @@ builder.Services.AddSingleton<IBookService, BookService>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/books", (IBookService bookService) =>
+    TypedResults.Ok(bookService.GetBooks()))
+    .WithName("GetBooks")
+    .WithOpenApi(x => new OpenApiOperation(x)
+    {
+        Summary = "Get Library Books",
+        Description = "Returns information about all the available books from the Amy's library.",
+        Tags = new List<OpenApiTag> { new() { Name = "Amy's Library" } }
+    });
 
 app.Run();
